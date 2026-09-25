336c91ba0e8e390b3cfad45795387185ffbd91ee
hermes-local <hermes-local@fix>
2026-09-25 07:35:46 +0300

    Point conftest + cli wrapper at hermes_telemetry/ after move

--- conftest.py
+++ conftest.py
@@ -16,7 +16,7 @@ ROOT = Path(__file__).parent
 
 # Register 'hermes_telemetry' as a package pointing at this directory.
 pkg = types.ModuleType("hermes_telemetry")
-pkg.__path__ = [str(ROOT)]  # allows submodule discovery
+pkg.__path__ = [str(ROOT / "hermes_telemetry")]  # allows submodule discovery
 pkg.__package__ = "hermes_telemetry"
 pkg.__file__ = str(ROOT / "__init__.py")
 sys.modules["hermes_telemetry"] = pkg
